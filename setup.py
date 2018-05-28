d590e3b002c997ff2a769b84f392de9984208c81
Matt Andrews <mandrews1026@gmail.com>
2018-05-28 13:05:53 -0700

    Fix subpackages not installing

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name='pybet',
     version='0.2',
-    packages=['pybet'],
+    packages=find_packages(),
     install_requires=['requests', 'bs4'],
     entry_points={
         'console_scripts': ['pybet=pybet.__main__:main']
